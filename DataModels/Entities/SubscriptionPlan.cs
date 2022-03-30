3b8385c2b6b3fa8ce9d30ad35c5d695f85e9365d
Vishal-Modi1 <modivishalp@gmail.com>
2022-03-30 09:44:25 +0530

    Added changes to manage subscription plans

---
+++ DataModels/Entities/SubscriptionPlan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace DataModels.Entities
+{
+    public class SubscriptionPlan
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ModuleIds { get; set; }
+
+        public bool IsCombo { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Column("Duration(InMonths)")]
+        public Int16 Duration { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public long CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public long? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public long? DeletedBy { get; set; }
+    }
+}
---
+++ DataModels/VM/SubscriptionPlan/SubscriptionPlanDataVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataModels.VM.SubscriptionPlan
+{
+    public class SubscriptionPlanDataVM
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ModuleIds { get; set; }
+
+        public string ModulesName { get; set; }
+        public bool IsCombo { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Column("Duration(InMonths)")]
+        public Int16 Duration { get; set; }
+
+        [NotMapped]
+        public bool IsLoadingEditButton { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int TotalRecords { get; set; }
+    }
+}
