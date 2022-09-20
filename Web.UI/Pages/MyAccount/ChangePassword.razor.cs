4d95936bc9898c240105943abb82c0b2fd2d2a75
Hardikmasalawala88 <Hardikmasalawala88@gmail.com>
2022-09-21 03:14:25 +0530

    19-imagepng 18-change-password

--- Web.UI/Pages/MyAccount/ChangePassword.razor.cs
+++ Web.UI/Pages/MyAccount/ChangePassword.razor.cs
@@ -4,6 +4,7 @@ using Web.UI.Utilities;
 using Microsoft.AspNetCore.Components;
 using DataModels.Constants;
 using Microsoft.AspNetCore.Components.Authorization;
+using System.Text.RegularExpressions;
 
 namespace Web.UI.Pages.MyAccount
 {
@@ -12,8 +13,15 @@ namespace Web.UI.Pages.MyAccount
         public string id { get; set; }
         private ChangePasswordVM changePasswordVM = new ChangePasswordVM();
 
+        private int _passwordStrengthCounter;
+        private const string UpperCasePattern = @"(?=.*[A-Z])";
+        private const string LowerCasePattern = @"(?=.*[a-z])";
+        private const string DigitPattern = @"(?=.*\d)";
+        private const string SpecialCharacterPattern = @"(?=.*[-+_!@#$%^&*., ?])";
+
         protected override async Task OnInitializedAsync()
         {
+            changePasswordVM.NewPassword = String.Empty;
             var user = (await AuthStat).User;
 
             if (!user.Identity.IsAuthenticated)
@@ -40,6 +48,53 @@ namespace Web.UI.Pages.MyAccount
 
             changePasswordVM = new ChangePasswordVM();
             changePasswordVM.UserId = Convert.ToInt64(id);
+        }
+
+        private bool PasswordValid()
+        {
+            _passwordStrengthCounter = 0;
+            bool passwordValid = false;
+            if (changePasswordVM.NewPassword is not null)
+            {
+                bool passwordLengthValid = changePasswordVM.NewPassword.Length >= 8;
+
+                if (Regex.Match(changePasswordVM.NewPassword, LowerCasePattern).Success)
+                {
+                    _passwordStrengthCounter++;
+                }
+
+                if (Regex.Match(changePasswordVM.NewPassword, UpperCasePattern).Success)
+                {
+                    _passwordStrengthCounter++;
+                }
+
+                if (Regex.Match(changePasswordVM.NewPassword, DigitPattern).Success)
+                {
+                    _passwordStrengthCounter++;
+                }
+
+                if (Regex.Match(changePasswordVM.NewPassword, SpecialCharacterPattern).Success && _passwordStrengthCounter < 3)
+                {
+                    _passwordStrengthCounter++;
+                }
+
+                if (_passwordStrengthCounter >= 3 && passwordLengthValid)
+                {
+                    passwordValid = true;
+                }
+
+                if (passwordLengthValid)
+                {
+                    _passwordStrengthCounter++;
+                }
+
+                if (changePasswordVM.NewPassword != changePasswordVM.ConfirmPassword)
+                {
+                    passwordValid = false;
+                }
+
+            }
+            return passwordValid;
         }
     }
 }
